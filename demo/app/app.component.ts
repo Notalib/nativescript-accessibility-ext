b37f9d754ba5af4e18506b89b321cbca128d6e78
Morten Anton Bach Sjøgren <msn@nota.dk>
2016-11-02 14:12:45 +0100

    chor: enable scss in demo

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "my-app",
-  templateUrl: "app.component.html",
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: [
+    'app.component.css',
+  ]
 })
 export class AppComponent {
 }

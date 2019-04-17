b632747db7e4b00bdf1f848a01a0998920f97a62
Daniel Freiling <ddfreiling@gmail.com>
2019-04-17 14:52:02 +0200

    travis update

--- demo-ng/references.d.ts
+++ demo-ng/references.d.ts
@@ -1,3 +1,4 @@
-/// <reference path="./node_modules/tns-core-modules/tns-core-modules.d.ts" /> Needed for autocompletion and compilation.
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android-28.d.ts" />
+/// <reference path="./node_modules/@nota/nativescript-accessibility-ext/ui/core/view.d.ts" />
+/// <reference path="./node_modules/@nota/nativescript-accessibility-ext/ui/page/page-ext.d.ts" />

ec634642880fd78b8d604bcd41d037107dbfea4e
Morten Anton Bach Sjøgren <msn@nota.dk>
2019-06-26 10:38:51 +0200

    fix: forgot to export the directives and pipes

--- src/angular/index.ts
+++ src/angular/index.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import '../index';
 import { A11YGridLayoutDirective } from './directives/a11y-grid-layout.directive';
-import { a11yFontScalingFactory, a11yFontScalingToken } from './font-scaling';
+import { a11yFontScalingFactory, A11YFontScalingObservable, a11yFontScalingToken } from './font-scaling';
 import { A11YFontScalePipe } from './pipes/a11y-font-scaling.pipe';
 
 @NgModule({
@@ -13,7 +13,8 @@ import { A11YFontScalePipe } from './pipes/a11y-font-scaling.pipe';
     A11YFontScalePipe,
   ],
   declarations: [A11YGridLayoutDirective, A11YFontScalePipe],
+  exports: [A11YGridLayoutDirective, A11YFontScalePipe],
 })
 export class A11YModule {}
 
-export { a11yFontScalingToken as fontScalingToken };
+export { A11YFontScalePipe, A11YFontScalingObservable, a11yFontScalingToken };

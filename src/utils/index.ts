9403cb85eb5be2a92d799214965ada61e71c4a52
Morten Anton Bach Sjøgren <msn@nota.dk>
2020-02-21 11:29:16 +0100

    minor import/export fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,5 @@ import * as utils from './utils';
 export * from './FontScaleObservable';
 export * from './global-events';
 export * from './helpers';
+
 export { utils };
--- src/views.ts
+++ src/views.ts
@@ -1,6 +1,7 @@
-import './ui/action-bar/action-bar';
 import './ui/core/view';
 import './ui/css-classes-helper';
 import './ui/slider/slider';
 
 export * from './ui/core/view';
+export * from './ui/css-classes-helper';
+export * from './ui/slider/slider';

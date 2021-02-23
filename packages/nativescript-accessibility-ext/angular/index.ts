2909badbf8a75fc6261254bb6581cb5998305def
Morten Sjøgren <m_abs@mabs.dk>
2021-02-23 16:48:37 +0100

    fix: missing export in angular

--- packages/nativescript-accessibility-ext/angular/index.ts
+++ packages/nativescript-accessibility-ext/angular/index.ts
@@ -5,7 +5,7 @@ import { A11yServiceEnabledObservable } from './data/a11y-service-enabled';
 import { A11YGridLayoutDirective } from './directives/a11y-grid-layout.directive';
 import { A11YFontScalePipe } from './pipes/a11y-font-scaling.pipe';
 export * from '@nota/nativescript-accessibility-ext';
-export { A11YFontScalePipe, A11yFontScalingObservable, A11yServiceEnabledObservable };
+export { A11YFontScalePipe, A11yFontScalingObservable, A11YGridLayoutDirective, A11yServiceEnabledObservable };
 
 @NgModule({
   providers: [A11YFontScalePipe],
